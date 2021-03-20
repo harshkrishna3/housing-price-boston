4e5d55c3a37d963a4a512c35768d02b977f64813
harshkrishna3 <harshkrishna3@gmail.com>
2021-03-21 01:10:12 +0530

    wrote program

---
+++ main.py
@@ -0,0 +1,33 @@
+#import dependencies
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+#import boston data
+raw_data = load_boston()
+print(raw_data)
+X = pd.DataFrame(raw_data['data'], columns = raw_data['feature_names'])
+print(X.head())
+y = pd.DataFrame(raw_data['target'])
+print(y.head())
+
+#preprocessing
+scalar = StandardScaler()
+X = pd.DataFrame(scalar.fit_transform(X), columns = raw_data['feature_names'])
+print(X.head())
+
+#split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+#Training model
+reg = LinearRegression()
+reg.fit(X_train, y_train)
+
+#checking accuracy
+y_pred = reg.predict(X_test)
+print('Mean squared error:', mean_squared_error(y_test, y_pred))
+print("accuracy:", reg.score(X_test, y_test))
