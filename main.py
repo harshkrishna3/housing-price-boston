fa17cd18672c0d3c3a3f69ea7a923d278f7c1095
harshkrishna3 <harshkrishna3@gmail.com>
2021-03-22 15:23:40 +0530

    divided code into cells

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#%%codecell
 #import dependencies
 from sklearn.datasets import load_boston
 from sklearn.linear_model import LinearRegression
@@ -7,6 +8,7 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 
+#%%codecell
 #import boston data
 raw_data = load_boston()
 print(raw_data)
@@ -15,18 +17,22 @@ print(X.head())
 y = pd.DataFrame(raw_data['target'])
 print(y.head())
 
+#%%codecell
 #preprocessing
 scalar = StandardScaler()
 X = pd.DataFrame(scalar.fit_transform(X), columns = raw_data['feature_names'])
 print(X.head())
 
+#%%codecell
 #split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+#%%codecell
 #Training model
 reg = LinearRegression()
 reg.fit(X_train, y_train)
 
+#%%codecell
 #checking accuracy
 y_pred = reg.predict(X_test)
 print('Mean squared error:', mean_squared_error(y_test, y_pred))
